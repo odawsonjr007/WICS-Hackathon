74fe2d209a1485aae9e1515c24f4a61e458cd4c1
odawsonjr007 <odawsonjr@gmail.com>
2026-03-21 10:15:15 -0500

    Js

---
+++ scripts.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function ()
+{
+    const deedBtn = document.getElementById("deedBtn");
+    const deedDisplay = document.getElementById("deedDisplay");
+    const deeds = [
+        "Compliment 3 different people (don't be vague!",
+        "Mentor someone",
+        "Pay for the next person in line",
+        "Donate something you don't need",
+        "Create an appreciation post for something you love",
+        "Run an errand for someone",
+        "Volunteer your time for community service",
+        "Send a quick message to a friend or someone you haven't talked to in a while",
+        "Greet a stranger",
+        "Help someone with homework",
+        "Encourage someone on their goals or taking a risk",
+        "Let go of a grudge and forgive that person",
+        "Create something innovative and gift it to someone",
+    ]
+    deedBtn.addEventListener("click, function");
+    
+}
